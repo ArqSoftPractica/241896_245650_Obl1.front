1830ea51e87c0d4f678b7b6d82be4d393fd783d8
fernandasecinaroinnovant <91079329+fernandasecinaroinnovant@users.noreply.github.com>
2022-09-18 16:09:38 -0300

    footer

---
+++ src/layouts/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <Box sx={{ p: 3, textAlign: 'center' }}>
+      <Typography>
+        © 2022 All rights reserved by Fernanda Secinaro (245650) - Juan Pablo Rodríguez Sotto (241896)
+      </Typography>
+    </Box>
+  );
+};
+
+export default Footer;
