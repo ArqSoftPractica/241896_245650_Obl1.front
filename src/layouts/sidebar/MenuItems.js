b6bd7444debd479bd61483173887085a771546c8
fernandasecinaroinnovant <91079329+fernandasecinaroinnovant@users.noreply.github.com>
2022-09-18 16:10:30 -0300

    template

---
+++ src/layouts/sidebar/MenuItems.js
@@ -0,0 +1,19 @@
+const Menuitems = [
+  {
+    title: 'Forms',
+    icon: 'layout',
+    href: '/forms',
+  },
+  {
+    title: 'Pagination',
+    icon: 'user',
+    href: '/pagination',
+  },
+  {
+    title: 'Tables',
+    icon: 'grid',
+    href: '/',
+  },
+];
+
+export default Menuitems;
