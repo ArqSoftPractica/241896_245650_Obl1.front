9f14d4222cbef11203817bff1c33951cd16190a5
fernandasecinaroinnovant <91079329+fernandasecinaroinnovant@users.noreply.github.com>
2022-09-18 16:04:45 -0300

    refactor

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../src/theme/theme';
+import createEmotionCache from '../src/createEmotionCache';
+import FullLayout from '../src/layouts/FullLayout';
+import '../styles/style.css';
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Flexy NextJs Starter kit page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <FullLayout>
+          <Component {...pageProps} />
+        </FullLayout>
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
