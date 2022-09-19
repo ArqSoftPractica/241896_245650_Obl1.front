09f4c6e186440e522a471ff4f0f0c56a8634637e
fernandasecinaroinnovant <91079329+fernandasecinaroinnovant@users.noreply.github.com>
2022-09-19 00:30:56 -0300

    Refactor

--- src/components/baseCard/BaseCard.tsx
+++ src/components/baseCard/BaseCard.tsx
@@ -1,16 +1,18 @@
 import { Card, CardContent, Box, Typography } from '@mui/material';
 
 export interface Props {
-  title: string;
+  title?: string;
   children: React.ReactNode;
 }
 
 const BaseCard: React.FC<Props> = ({ title, children }) => {
   return (
     <Card>
-      <Box p={2} display="flex" alignItems="center">
-        <Typography variant="h4">{title}</Typography>
-      </Box>
+      {title && (
+        <Box p={2} display="flex" alignItems="center" justifyContent="space-between" style={{ margin: '0 30px' }}>
+          <Typography variant="h4">{title}</Typography>
+        </Box>
+      )}
       <CardContent>{children}</CardContent>
     </Card>
   );
