f30cadfd88c86d1254d5ea5a0ab2589a210a5ad4
fernandasecinaro <secinarof@gmail.com>
2022-09-30 22:25:17 -0300

    signup connection

---
+++ src/services/register.service.ts
@@ -0,0 +1,38 @@
+import InvalidRequestError from 'src/services/errors/InvalidRequest';
+
+export interface SignUpRequest {
+  email: string;
+  password: string;
+  familyName: string;
+  name: string;
+}
+
+export interface Response {
+  message: string;
+}
+
+export const signUp = (request: SignUpRequest): Promise<Response> => {
+  const signUpResponse = fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users`, {
+    method: 'POST',
+    body: JSON.stringify(request),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(async (response) => {
+      const parsedResponse: Response = await response.json();
+      if (!response.ok) {
+        throw new InvalidRequestError(parsedResponse.message);
+      }
+      return parsedResponse;
+    })
+    .then((data) => data)
+    .catch((error) => {
+      console.error(error);
+      if (error instanceof InvalidRequestError) {
+        throw error;
+      }
+      throw new Error('Oops! Something went wrong. Please try again later.');
+    });
+  return signUpResponse;
+};
