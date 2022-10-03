df9a25e55ae409b0a4ade1d097328eeff318d6df
fernandasecinaro <secinarof@gmail.com>
2022-10-03 00:02:49 -0300

    fix refresh chart

--- src/components/categories/chart/CategoriesChart.tsx
+++ src/components/categories/chart/CategoriesChart.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import { getExpensesPerCategory } from 'src/services/expenses.service';
-import { toast } from 'react-toastify';
 import formatDate from 'src/utils/formatDate';
 import BaseCard from '../../baseCard/BaseCard';
 import chartOptions from './ChartConfig';
@@ -11,25 +9,12 @@ const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 export interface Props {
   fromDate: Date;
   toDate: Date;
+  chartData: ApexAxisChartSeries | ApexNonAxisChartSeries | undefined;
+  categories: string[] | undefined;
 }
 
-const CategoriesChart: React.FC<Props> = ({ fromDate, toDate }) => {
-  const [chartData, setChartData] = React.useState<ApexAxisChartSeries | ApexNonAxisChartSeries | undefined>();
-  const [categories, setCategories] = React.useState<string[]>();
+const CategoriesChart: React.FC<Props> = ({ fromDate, toDate, chartData, categories }) => {
   const [categoriesChartOptions, setCategoriesChartOptions] = React.useState(chartOptions);
-
-  useEffect(() => {
-    getExpensesPerCategory({ fromDate, toDate })
-      .then(({ expensesPerCategory }) => {
-        const data = expensesPerCategory.length > 0 ? expensesPerCategory.map(({ totalAmount }) => +totalAmount) : [];
-        const categoriesData = expensesPerCategory.length > 0 ? expensesPerCategory.map(({ name }) => name) : [];
-        setChartData([{ data, name: 'Total Amount' }]);
-        setCategories(categoriesData);
-      })
-      .catch((err) => {
-        toast.error(err.message);
-      });
-  }, [fromDate, toDate]);
 
   const periodOfData = `${formatDate(fromDate)} - ${formatDate(toDate)}`;
 
